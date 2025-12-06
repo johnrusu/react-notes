f0153dfd6b4a669439a05d9b0102f7e82ccec1c4
Rusu Ionut <johnrusu@gmail.com>
2025-12-06 22:11:01 +0200

    fix: github actions

--- src/main.tsx
+++ src/main.tsx
@@ -29,7 +29,7 @@ createRoot(document.getElementById("root")!).render(
   >
     <Provider store={store}>
       <StrictMode>
-        <BrowserRouter basename={`/${BASE_NAME.replace(/\/$/, "")}`}>
+        <BrowserRouter basename={`/${BASE_NAME}`}>
           <App />
         </BrowserRouter>
       </StrictMode>
